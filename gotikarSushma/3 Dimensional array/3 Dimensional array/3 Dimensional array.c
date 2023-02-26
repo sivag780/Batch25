581f7047ad3b38ce4f1829d926dab659070ec837
sushma <gotikarsushma@gmail.com>
2023-02-26 17:07:11 +0530

    added 3d array

---
+++ gotikarSushma/3 Dimensional array/3 Dimensional array/3 Dimensional array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a[2][2][3]={{{1,2,3},{2,3,4}},
+	                {{4,5,6},{7,8,9}}};
+	int i,j,k;
+for(i=0;i<2;i++)
+{
+	for(j=0;j<2;j++)
+	{
+		for(k=0;k<3;k++)
+		{
+			printf("%d\n",a[i][j][k]);
+			printf("%d\n",a[i][j][k],*(*(*(a+i)+j)+k));
+			printf("%d\n",&a[i][j][k],(*(*(a+i)+j)+k));
+
+		}
+	}
+}
+}
