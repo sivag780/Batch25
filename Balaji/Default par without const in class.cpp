2773a29bbfab0b2424bc7b1834065ccdb99ed02c
bandibalajireddy <bbalajireddy99@gmail.com>
2023-02-26 11:32:55 +0530

    add new line in the prog

---
+++ Balaji/Default par without const in class.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+//using namespace std;
+//class A
+//{
+//	int x;
+//	int y;
+//	public:
+//	void assign(int a, int b=88)
+//	{
+//		x=a;
+//		y=b;
+//	} 
+//	void display()
+//	{
+//		cout<<x<<" "<<y<<endl;
+//	}
+//};
+//int main()
+//{
+//	A obj;
+//	obj.assign(55,33);
+//	obj.display();
+//}
