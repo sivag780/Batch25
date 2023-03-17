74f8b3917156f8d587b63494eb415bf867c775d7
Nagaraj-2023 <nagaraj.eeeb@gmail.com>
2023-03-17 11:43:10 +0530

    abc

---
+++ cp2/static.cpp
@@ -0,0 +1,62 @@
+////#include<iostream>
+////using namespace std;
+////
+////class A
+////{
+////	int x,y;
+////	static int z;
+////public:
+////	A()
+////	{
+////		x=10;
+////		y=20;
+////		z=30;
+////	}
+////	void display()
+////	{
+////		cout<<x<<" "<<y<<" "<<z<<endl;
+////	}
+////};
+////int A::z;
+////int main()
+////{
+////	A obj1;
+////	obj1.display();
+////	return 0;
+////}
+//
+//
+//
+//
+//
+//
+//
+//
+////#include<iostream>
+////using namespace std;
+////
+////class S
+////{
+////public:
+////	int x;
+////	static int y;
+////
+////	S()
+////	{
+////		x=10;
+////		y=20;
+////	}
+////	 static void display()
+////	{
+////		//cout<<S::y<<endl;
+////		cout<<y<<endl;
+////	}
+////};
+////int S::y=30;
+////int main()
+////{
+////	S obj;
+////	cout<<obj.x<<" "<<obj.y<<endl;
+////	obj.display();
+////	
+////}
